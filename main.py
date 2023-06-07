7069fde7d3b11bcb382f56756f473aac3dd3ea05
jocarrojas <97867812+jocarrojas@users.noreply.github.com>
2023-06-07 00:15:00 +0000

    Range use in for loop

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+# Range use in for loop
 start = int(input("Give me a starting number: "))
 end = int(input("Give me a number to end before: "))
 inc = int(input("How much to increase each time? "))
